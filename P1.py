98b9540684f0862865cadc996f4ae0c7a17cec22
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-10-13 01:29:59 +0600

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,6 @@
+x = input("First number: ")
+y = input("\nSecond number: ")
+
+z = float(x) + float(y)
+
+print("The sum of {0} and {1} is {2}" .format(x, y, z))
---
+++ P5.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a number: "))
+if x % 2 ==0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
